4dc79392b3f7eb7defc59d4b7b5c4249d4dafa28
kokeb <50007097+bethel4@users.noreply.github.com>
2024-02-04 15:37:49 +0300

    new changes for content moderation

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/user_dashboard/uploads.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/user_dashboard/uploads.js
@@ -6,22 +6,28 @@
 //
 // Invenio App RDM is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
-import ReactDOM from 'react-dom';
-
+import ReactDOM from "react-dom";
 
 import { i18next } from "@translations/invenio_app_rdm/i18next";
 import _get from "lodash/get";
 import _truncate from "lodash/truncate";
 import React from "react";
 import { Button, Card, Divider, Header, Segment } from "semantic-ui-react";
+import {
+  ModalHeader,
+  ModalContent,
+  ModalActions,
+  Icon,
+  Modal,
+} from "semantic-ui-react";
 import { parametrize, overrideStore } from "react-overridable";
-  import {
-    RDMCountComponent,
-    RDMEmptyResults as RDMNoSearchResults,
-    RDMRecordSearchBarElement,
-    RDMToggleComponent,
-  } from "../search/components";
-import { http,withCancel, } from "react-invenio-forms";
+import {
+  RDMCountComponent,
+  RDMEmptyResults as RDMNoSearchResults,
+  RDMRecordSearchBarElement,
+  RDMToggleComponent,
+} from "../search/components";
+import { http, withCancel } from "react-invenio-forms";
 import { DashboardResultView, DashboardSearchLayoutHOC } from "./base";
 import { createSearchAppInit } from "@js/invenio_search_ui";
 import { ComputerTabletUploadsItem } from "./uploads_items/ComputerTabletUploadsItem";
@@ -32,16 +38,20 @@ import {
   ContribBucketAggregationElement,
   ContribBucketAggregationValuesElement,
 } from "@js/invenio_search_ui/components";
-import {CommunityApi} from "./CommunityApi"
+import { CommunityApi } from "./CommunityApi";
 const statuses = {
   in_review: { color: "warning", title: i18next.t("In review") },
   declined: { color: "negative", title: i18next.t("Declined") },
   expired: { color: "expired", title: i18next.t("Expired") },
   draft_with_review: { color: "neutral", title: i18next.t("Draft") },
   draft: { color: "neutral", title: i18next.t("Draft") },
-  new_version_draft: { color: "neutral", title: i18next.t("New version draft") },
+  new_version_draft: {
+    color: "neutral",
+    title: i18next.t("New version draft"),
+  },
 };
 
+let status = false;
 export const RDMRecordResultsListItem = ({ result }) => {
   const editRecord = () => {
     http
@@ -51,7 +61,7 @@ export const RDMRecordResultsListItem = ({ result }) => {
         {
           headers: {
             "Content-Type": "application/json",
-            "Accept": "application/vnd.inveniordm.v1+json",
+            Accept: "application/vnd.inveniordm.v1+json",
           },
         }
       )
@@ -66,8 +76,16 @@ export const RDMRecordResultsListItem = ({ result }) => {
   const isPublished = result.is_published;
   const access = {
     accessStatusId: _get(result, "ui.access_status.id", i18next.t("open")),
-    accessStatus: _get(result, "ui.access_status.title_l10n", i18next.t("Open")),
-    accessStatusIcon: _get(result, "ui.access_status.icon", i18next.t("unlock")),
+    accessStatus: _get(
+      result,
+      "ui.access_status.title_l10n",
+      i18next.t("Open")
+    ),
+    accessStatusIcon: _get(
+      result,
+      "ui.access_status.icon",
+      i18next.t("unlock")
+    ),
   };
   const uiMetadata = {
     descriptionStripped: _get(
@@ -92,10 +110,14 @@ export const RDMRecordResultsListItem = ({ result }) => {
     version: result.ui?.version ?? "",
     isPublished: isPublished,
     viewLink: isPublished ? `/records/${result.id}` : `/uploads/${result.id}`,
-    publishingInformation: _get(result, "ui.publishing_information.journal", ""),
+    publishingInformation: _get(
+      result,
+      "ui.publishing_information.journal",
+      ""
+    ),
   };
-  
-  deleteCommunity(result)
+
+  deleteCommunity(result);
   return (
     <>
       <ComputerTabletUploadsItem
@@ -114,29 +136,35 @@ export const RDMRecordResultsListItem = ({ result }) => {
       />
     </>
   );
-  
 };
 
 RDMRecordResultsListItem.propTypes = {
   result: PropTypes.object.isRequired,
 };
-const deleteCommunity =async (result) =>{
-  if(result.deletion_status.is_deleted==false){
-  // const {request} = this.props;
+const deleteCommunity = async (result) => {
+  if (result.deletion_status.is_deleted == false) {
+    // const {request} = this.props;
     // console.log(request.expanded.receiver.id)
-    let communityId = result.parent.communities.default
+    let communityId = result.parent.communities.default;
     let baseUrl = "/api/communities";
-    console.log("this is the community" + communityId)
-    return http.delete(`${baseUrl}/${communityId}`, {
-      
-    });
+    console.log("this is the community" + communityId);
+    return http.delete(`${baseUrl}/${communityId}`, {});
   }
-  }
+};
 // FIXME: Keeping ResultsGrid.item and SearchBar.element because otherwise
 // these components in RDM result broken.
 
 export const RDMRecordResultsGridItem = ({ result, index }) => {
-  const descriptionStripped = _get(result, "ui.description_stripped", "No description");
+  // useEffect(() => {
+  //   return () => {
+  //     fetchRequest(result.id)
+  //   };
+  // }, []);
+  const descriptionStripped = _get(
+    result,
+    "ui.description_stripped",
+    "No description"
+  );
   return (
     <Card fluid key={index} href={`/records/${result.id}`}>
       <Card.Content>
@@ -161,61 +189,78 @@ export const communityErrorSerializer = (error) => ({
 });
 
 const getSlug = async () => {
-     
   // this.handleOpen()
 
   //  console.log(JSON.stringify(data))
   let h = {
-   "Accept": "application/json",
-   "Content-Type": "application/json",
-  //  "Authorization": "Bearer"  + token
-  }
- 
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    //  "Authorization": "Bearer"  + token
+  };
+
   await http
     .post(
-       'https://127.0.0.1:5000/api/records/communites',
-     {},
+      "https://127.0.0.1:5000/api/records/communites",
+      {},
       {
         h: {
-          "Accept": "application/json",
+          Accept: "application/json",
           "Content-Type": "application/json",
         },
         withCredentials: true,
       }
     )
     .then(async (resp) => {
-      let slug = resp.data.identifer
+      let slug = resp.data.identifer;
       let cancellableCreate = null; // Declare cancellableCreate using useState if needed in other hooks
       const client = new CommunityApi();
-      const payload ={
-        "access": {
-            "visibility": "public"
+      const payload = {
+        access: {
+          visibility: "public",
         },
-        "slug": resp.data.identifer,
-        "metadata": {
-            "title": resp.data.institution
-        }
-      }
+        slug: resp.data.identifer,
+        metadata: {
+          title: resp.data.institution,
+        },
+      };
       try {
-        console.log("this is the pyload " + payload)
-        cancellableCreate = withCancel(client.create(payload));
-        const response = await cancellableCreate.promise;
-        window.location.href = `/uploads/new?community=${slug}`;
-        console.log(response.data.links);
+        if (slug === "null") {
+          window.location.href = "/uploads/new";
+        } else if (slug === "none") {
+          status = true;
+          console.log("this is the pyload " + resp);
+        } else {
+          console.log("this is the pyload " + resp);
+          cancellableCreate = withCancel(client.create(payload));
+          const response = await cancellableCreate.promise;
+          window.location.href = `/uploads/new?community=${slug}`;
+          console.log(response.data.links);
+        }
+
         // Perform further actions based on the response
       } catch (error) {
-        if (error === 'UNMOUNTED') return;
-  
+        if (error === "UNMOUNTED") return;
+
         const { errors, message } = communityErrorSerializer(error);
-  
-        
       }
     });
 };
 
-
-
 export const RDMEmptyResults = (props) => {
+  const [state, dispatch] = React.useReducer(modaReducer, {
+    open: false,
+    size: undefined,
+  });
+  const modaReducer = (state, action) => {
+    switch (action.type) {
+      case "close":
+        return { open: false };
+      case "open":
+        return { open: true, size: action.size };
+      default:
+        throw new Error("Unsupported action...");
+    }
+  };
   const { queryString } = props;
   return queryString === "" ? (
     <Segment.Group>
@@ -223,7 +268,9 @@ export const RDMEmptyResults = (props) => {
         <Header as="h1" align="center">
           <Header.Content>
             {i18next.t("Get started!")}
-            <Header.Subheader>{i18next.t("Make your first upload!")}</Header.Subheader>
+            <Header.Subheader>
+              {i18next.t("Make your first upload!")}
+            </Header.Subheader>
           </Header.Content>
         </Header>
         <Divider hidden />
@@ -232,15 +279,31 @@ export const RDMEmptyResults = (props) => {
           icon="upload"
           floated="right"
           // href="/uploads/new"
-          onClick = {()=>{getSlug()}}
+          onClick={() => {
+            dispatch({ type: "open", size: "small" });
+            getSlug();
+          }}
           content={i18next.t("New upload")}
         />
+        <Modal
+          size={state.size}
+          open={state.open}
+          onClose={() => dispatch({ type: "close" })}
+        >
+          <ModalHeader>Publication Approval</ModalHeader>
+          <ModalContent>
+            <p>Only other reviewers can review your work</p>
+          </ModalContent>
+          <ModalActions>
+            <Button positive onClick={() => dispatch({ type: "close" })}>
+              Close
+            </Button>
+          </ModalActions>
+        </Modal>
       </Segment>
     </Segment.Group>
   ) : (
     <Segment padded="very">
- 
-
       <RDMNoSearchResults {...props} searchPath="/me/uploads" />
     </Segment>
   );
@@ -258,28 +321,66 @@ export const DashboardUploadsSearchLayout = DashboardSearchLayoutHOC({
     <Button
       positive
       icon="upload"
-      // href="/uploads/new"
-      onClick = {()=>{getSlug()}}
+      onClick={() => {
+        getSlug();
+      }}
       content={i18next.t("New upload")}
       floated="right"
     />
   ),
   appName: appName,
 });
+
 export const UplodButton = () => {
-  
-  return (  
-      <>
+  // Define modaReducer before using it
+  const modaReducer = (state, action) => {
+    switch (action.type) {
+      case "close":
+        return { open: false };
+      case "open":
+        return { open: true, size: action.size };
+      default:
+        throw new Error("Unsupported action...");
+    }
+  };
+
+  const [state, dispatch] = React.useReducer(modaReducer, {
+    open: false,
+    size: undefined,
+  });
+
+  return (
+    <>
       <Button
-          positive
-          icon="upload"
-          floated="right"
-          // href="/uploads/new"
-          onClick = {()=>{getSlug()}}
-          content={i18next.t("New upload")}
-        />
-      </>
-  )
+        positive
+        icon="upload"
+        floated="right"
+        onClick={() => {
+          // Assuming getSlug is defined somewhere in your code
+          getSlug();
+          dispatch({ type: "open", size: "small" });
+        }}
+        content={i18next.t("New upload")}
+      />
+      {status && (
+        <Modal
+          size={state.size}
+          open={state.open}
+          onClose={() => dispatch({ type: "close" })}
+        >
+          <ModalHeader> Restriction on Records Reviews</ModalHeader>
+          <ModalContent>
+            <p>Your records can only be reviewed by other reviewers</p>
+          </ModalContent>
+          <ModalActions>
+            <Button positive onClick={() => dispatch({ type: "close" })}>
+              Close
+            </Button>
+          </ModalActions>
+        </Modal>
+      )}
+    </>
+  );
 };
 
 const ContribSearchAppFacetsWithConfig = parametrize(ContribSearchAppFacets, {
@@ -292,7 +393,8 @@ const DashboardResultViewWAppName = parametrize(DashboardResultView, {
 
 export const defaultComponents = {
   [`${appName}.BucketAggregation.element`]: ContribBucketAggregationElement,
-  [`${appName}.BucketAggregationValues.element`]: ContribBucketAggregationValuesElement,
+  [`${appName}.BucketAggregationValues.element`]:
+    ContribBucketAggregationValuesElement,
   [`${appName}.Count.element`]: RDMCountComponent,
   [`${appName}.EmptyResults.element`]: RDMEmptyResults,
   [`${appName}.ResultsList.item`]: RDMRecordResultsListItem,
@@ -311,4 +413,4 @@ createSearchAppInit(
   "invenio-search-config",
   true
 );
-ReactDOM.render(<UplodButton />, document.getElementById('react-root'));
+ReactDOM.render(<UplodButton />, document.getElementById("upload-root"));
